6179f2fd1edfdabb9cc28bc9e4ed097c7f88bd76
matvey <matveyvs1987@gmail.com>
2023-11-02 11:22:03 +0100

    test: add tests for pageable and sorting

--- src/main/java/by/matveyvs/springdatajpatask/repository/UserRepository.java
+++ src/main/java/by/matveyvs/springdatajpatask/repository/UserRepository.java
@@ -2,6 +2,8 @@ package by.matveyvs.springdatajpatask.repository;
 
 import by.matveyvs.springdatajpatask.entity.Role;
 import by.matveyvs.springdatajpatask.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAllByRoleAndBirthDateBetween(Role role, LocalDate startDate, LocalDate endDate);
 
     List<User> findFirst4By(Sort sort);
+
+    Page<User> findAllByRole(Role role, Pageable pageable);
+
 }
--- src/test/java/by/matveyvs/springdatajpatask/repository/UserRepositoryTest.java
+++ src/test/java/by/matveyvs/springdatajpatask/repository/UserRepositoryTest.java
@@ -5,13 +5,13 @@ import by.matveyvs.springdatajpatask.entity.Role;
 import by.matveyvs.springdatajpatask.entity.User;
 import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @AllArgsConstructor
@@ -52,6 +52,48 @@ class UserRepositoryTest {
         assertFalse(test.isEmpty());
         for (int i = 0; i < test.size() - 1; i++) {
             assertTrue(test.get(i).getBirthDate().isBefore((test.get(i + 1).getBirthDate())));
+        }
+    }
+
+    @Test
+    void findByRoleUserPlusPaginationAndSortByUsername(){
+        var sort = Sort.by("username");
+        var pageable = PageRequest.of(0,1, sort);
+
+        var page = userRepository.findAllByRole(Role.USER, pageable);
+        assertEquals(2, page.getTotalPages());
+        assertEquals(2, page.getTotalElements());
+        page.forEach(user -> assertEquals(Role.USER, user.getRole()));
+        while (page.hasNext()){
+            page = userRepository.findAllByRole(Role.USER, page.nextPageable());
+            page.forEach(user -> assertEquals(Role.USER, user.getRole()));
+        }
+    }
+    @Test
+    void findByRoleAdminPlusPaginationAndSortById(){
+        var sort = Sort.by("id");
+        var pageable = PageRequest.of(0,1, sort);
+
+        var page = userRepository.findAllByRole(Role.ADMIN, pageable);
+        assertEquals(1, page.getTotalPages());
+        assertEquals(1, page.getTotalElements());
+        page.forEach(user -> assertEquals(Role.ADMIN, user.getRole()));
+        while (page.hasNext()){
+            page = userRepository.findAllByRole(Role.USER, page.nextPageable());
+            page.forEach(user -> assertEquals(Role.ADMIN, user.getRole()));
+        }
+    }
+    @Test
+    void findByRoleUserPlusPaginationAndSortByFirstName(){
+        var sort = Sort.by("id");
+        var pageable = PageRequest.of(0,2, sort);
+        var page = userRepository.findAllByRole(Role.USER, pageable);
+        assertEquals(1, page.getTotalPages());
+        assertEquals(2, page.getTotalElements());
+        page.forEach(user -> assertEquals(Role.USER, user.getRole()));
+        while (page.hasNext()){
+            page = userRepository.findAllByRole(Role.USER, page.nextPageable());
+            page.forEach(user -> assertEquals(Role.USER, user.getRole()));
         }
     }
 }
